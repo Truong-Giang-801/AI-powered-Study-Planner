da330ed6651bf6c999d27cd44b1ef686d2f192bc
Truong-Giang-801 <giangle8013@gmail.com>
2025-01-05 12:07:11 +0700

    fix warning

--- TaskManagementApp/obj/Debug/net9.0/TaskManagementApp.AssemblyInfo.cs
+++ TaskManagementApp/obj/Debug/net9.0/TaskManagementApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagementApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8084c7fa0da53ac5f3e4f1dda2d447f0fa9fdc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e435934530f3c93ee740e6fc081733b6beb234a")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagementApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagementApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
